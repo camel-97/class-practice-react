b2c881c7254d37add56840958cf68c5585a79a02
camel-97 <cfmcmillan@icloud.com>
2025-12-05 09:48:32 +0000

    populate coponents in app file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
+      <FunctionalInput name="Functional component!" />
+      <div className="divider" />
+      <ClassInput name="Class based component!" />
     </>
   )
 }
